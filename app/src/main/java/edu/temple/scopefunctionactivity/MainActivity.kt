af517f2ac7874686ce584812cd85596b6b2892e0
Matt Gibson <mattgibson333@gmail.com>
2024-10-03 12:16:49 -0400

    Second function refactored

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -36,14 +36,15 @@ class MainActivity : AppCompatActivity() {
 
     // Return true if average value in list is greater than median value, false otherwise
     private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
-        val avg = listOfNumbers.average()
-        val sortedList = listOfNumbers.sorted()
-        val median = if (sortedList.size % 2 == 0)
-            (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
-        else
-            sortedList[sortedList.size / 2]
-
-        return avg < median
+        listOfNumbers.let {
+            it.sorted()
+            if (it.size % 2 == 0) {
+                return it.average() > (it[it.size / 2] + it[(it.size - 1) / 2]) / 2
+            }
+            else {
+                return it.average() > it[it.size/2]
+            }
+        }
     }
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
